f05c3d039838717a8b0de63dcbc9a24169732150
badaev52sm <badaev52@gmail.com>
2022-11-22 23:01:40 +0300

    Квадрат числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int n1, n2;
+string? strn1, strn2;
+
+Console.Write("Введите число 1: ");
+strn1 = Console.ReadLine();
+    
+Console.WriteLine();
+
+Console.Write("Введите число 2: ");
+strn2 = Console.ReadLine();
+
+Console.WriteLine();
+
+n1 = Convert.ToInt32(strn1);
+n2 = Convert.ToInt32(strn2);
+
+int qn = n1 * n1;
+
+if (qn == n2){
+    Console.WriteLine("Квадрат числа (n1 * n1) = " + qn + " является квадратом числа n2: " + n2 );
+    
+}else{
+    Console.WriteLine("Квадрат числа (n1 * n1) = " + qn + " НЕ является квадратом числа n2: " + n2 );
+}
+
